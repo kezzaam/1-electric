dff78745c678a320b79f3af34f64b45320eee852
Keren Elizabeth <124431740+kezzaam@users.noreply.github.com>
2023-09-19 09:37:43 +1200

    Update next.config.js
    Still working on image fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-  
-   // Add basePath
-    basePath: '/github-pages',
-  
-  }
-  
-  module.exports = nextConfig
+  output: 'export',
+  reactStrictMode: true,
+
+ // Add basePath
+  basePath: '/github-pages',
+
+}
+
+module.exports = nextConfig
+
