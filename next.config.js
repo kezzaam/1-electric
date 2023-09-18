83e4a0e3a7a77bb88e20a3ede39f52388d1bdbfe
Keren Elizabeth <kezzaam@hotmail.com>
2023-09-19 10:14:00 +1200

    Commit 13
    Changes to github workflow and next config.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
+// production variable
+const isProd = process.env.NODE_ENV === 'production'
 
- // Add basePath
-  basePath: '/github-pages',
+const nextConfig = {
+  // Add basePath with condition based on production or development
+  // this is so that development running npm run dev still works
+  basePath: isProd ? '/1-electric' : '',
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
 
 }
 
