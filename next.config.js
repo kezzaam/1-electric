13c918f0bbba43169826f7a1169da00adb06c644
Keren Elizabeth <124431740+kezzaam@users.noreply.github.com>
2023-09-19 10:45:10 +1200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 // production variable
-const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  // Add basePath with condition based on production or development
-  // this is so that development running npm run dev still works
-  basePath: isProd ? '/1-electric' : '',
   output: 'export',
   distDir: 'dist',
   images: {
